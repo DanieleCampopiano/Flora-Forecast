b884271ae880f1a9c166d8f97d9d5e60f6425f6b
DanieleCampopiano <daniele.campopiano@outlook.it>
2023-06-29 20:31:00 +0200

    Custom Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Amplify } from 'aws-amplify';
-import { Authenticator  } from '@aws-amplify/ui-react';
+import { Authenticator, View, useTheme, Image, Text } from '@aws-amplify/ui-react';
 import awsExports from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 import SiteNav from './components/Common/SiteNav';
@@ -11,13 +10,38 @@ import {Route,Routes} from 'react-router-dom';
 import HomePage from './components/home/HomePage';
 import Contacts from './components/contacts/Contacts';
 
-
-
 Amplify.configure(awsExports);
 
 function App() {
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Contacts App"
+            src="/img/logo.png"
+          />
+        </View>
+      );
+    },
+
+    Footer() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; CRS 2023
+          </Text>
+        </View>
+      );
+    },
+  };
+
   return (
-    <Authenticator loginMechanisms={['email']} >
+    <Authenticator loginMechanisms={['email']} components={components}>
     {({ signOut, user }) => (
       <div>
         <SiteNav logOut={signOut} />
