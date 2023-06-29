87d68cff4ea3fa8aeaa8e03d9560435037361af0
DanieleCampopiano <daniele.campopiano@outlook.it>
2023-06-29 20:06:43 +0200

    Upload project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator  } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+import SiteNav from './components/Common/SiteNav';
+import SiteFooter from './components/Common/SiteFooteer';
+import {Route,Routes} from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import Contacts from './components/contacts/Contacts';
+
+
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator loginMechanisms={['email']} >
+    {({ signOut, user }) => (
+      <div>
+        <SiteNav logOut={signOut} />
+        <Routes>
+          <Route path='*' element={<HomePage />} />
+          <Route path='/' exact={true} element={<HomePage />} />
+          <Route path='/contacts' element={<Contacts />} />
+        </Routes>
+        <SiteFooter />
+      </div>
+    )}
+    </Authenticator>
   );
 }
 
