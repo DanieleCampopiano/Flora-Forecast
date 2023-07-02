df0f8a4869cb4a725c8ca206d4fa8778708ca16e
Mario Rivelli <m.rivelli1@studenti.unimol.it>
2023-07-02 20:54:51 +0200

    Update page Payment and Home

--- src/components/Common/SiteNav.js
+++ src/components/Common/SiteNav.js
@@ -1,27 +1,36 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function SiteNav(props) {
-    const handleLogout = () => {
-        props.logOut();
-    }    
+  const handleLogout = () => {
+    props.logOut();
+  };
 
-    return (
-        <header>
-            <Navbar bg="dark" expand="lg" variant="dark">
-                <Container>
-                    <Navbar.Brand>Flora Forecast</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-md-auto">
-                        <Nav.Link onClick={handleLogout}>Logout</Nav.Link> 
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
+  return (
+    <header>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="/img/logonav.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Flora Forecast
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-md-auto">
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
 export default SiteNav;
--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -1,54 +1,60 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 import "./Payment.css";
 
 function Payment() {
   const navigate = useNavigate();
-
-  const lambdaFunction = () => {
-    window.location.href = "https://google.it";
-  };
-
   return (
-    <div className="App-body">
-      <button onClick={lambdaFunction}>Processa Immagine Satellitare</button>
-      <h1>Make the payment</h1>
-      <img
-        height="200"
-        src="\img\World.gif"
-        alt="Sottotitolo"
-      />
-      <p>
-        <span className="book-price">29.99€</span>
-      </p>
-      <PayPalScriptProvider
-        options={{
-          "client-id":
-            "AfGHfJfSKXsoM_WU3SAgRZr9sPdywKPVX8I465Ch-2g-xO6YSXAp0cPX6kM6gorK01t5GUQPzdtkZXwU",
-        }}
-      >
-        <PayPalButtons
-          createOrder={(data, actions) => {
-            return actions.order.create({
-              purchase_units: [
-                {
-                  amount: {
-                    value: "00.01",
-                  },
-                },
-              ],
-            });
-          }}
-          onApprove={async (data, actions) => {
-            const details = await actions.order.capture();
-            const name = details.payer.name.given_name;
-            alert("Transaction completed by " + name);
-            
-            navigate('/homepage');
-          }}
-        />
-      </PayPalScriptProvider>
-    </div>
+    <Container>
+      <Row className="px-4 my-5">
+        <Col xs={4} sm={6}>
+          <Image src="/img/logo.png" width="500" height="500"/>
+        </Col>
+        <Col sm={6}>
+          <b className="price">Fatturazione</b>
+          <p className="explanation">
+            Benvenuti su Flora Forecast la piattaforma innovativa che utilizza
+            le riprese satellitari per prevedere la crescita della flora negli
+            ambienti urbani. Esplora e analizza l'evoluzione della vegetazione
+            nel tempo con heatmap animate. Paga per accedere a questa tecnologia
+            all'avanguardia.
+          </p>
+          <b className="price">10€</b>
+          <br />
+          <br />
+          <PayPalScriptProvider
+            options={{
+              "client-id":
+                "AfGHfJfSKXsoM_WU3SAgRZr9sPdywKPVX8I465Ch-2g-xO6YSXAp0cPX6kM6gorK01t5GUQPzdtkZXwU",
+            }}
+          >
+            <PayPalButtons
+              createOrder={(data, actions) => {
+                return actions.order.create({
+                  purchase_units: [
+                    {
+                      amount: {
+                        value: "00.01",
+                      },
+                    },
+                  ],
+                });
+              }}
+              onApprove={async (data, actions) => {
+                const details = await actions.order.capture();
+                const name = details.payer.name.given_name;
+                alert("Transazione completata da " + name);
+                navigate('/homepage');
+              }}
+            />
+          </PayPalScriptProvider>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
